4d2adde5a31ba3dcc96faea92b557b5961f1ea66
marvin.weiss <marvin.weiss@stud.th-luebeck.de>
2024-06-02 23:30:33 +0200

    newPlease enter the commit message for your changes. Lines starting new

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        //Calculator model = new Calculator();
+        PuzzleView view = new PuzzleView();
+       // CalcControler controler = new CalcControler(model, view);
+       // controler.handleEvents();
+        view.setVisible(true);
+    }
+    
+
+}
