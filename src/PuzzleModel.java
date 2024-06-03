a62244a5cf7bc911fcd660e47c5ad30e9d4283ac
marvin.weiss <marvin.weiss@stud.th-luebeck.de>
2024-06-03 15:30:00 +0200

    yes

--- src/PuzzleModel.java
+++ src/PuzzleModel.java
@@ -1,21 +1,43 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public class PuzzleModel {
 
-public static List<String> numShuffle() {
+    public static List<String> numShuffle() {
         List<String> numsList = new ArrayList<>();
-        
+
         for (int i = 1; i <= 15; i++) {
             numsList.add(Integer.toString(i));
         }
-        
+
         Collections.shuffle(numsList, new Random());
 
         return numsList;
     }
 
+    public static Map<Integer, Tuple<Integer, Integer>> winCoordinates() {
+        Map<Integer, Tuple<Integer, Integer>> m = new HashMap<>();
+        m.put(1, new Tuple<>(1, 1));
+        m.put(2, new Tuple<>(1, 2));
+        m.put(3, new Tuple<>(1, 3));
+        m.put(4, new Tuple<>(1, 4));
+        m.put(5, new Tuple<>(2, 1));
+        m.put(6, new Tuple<>(2, 2));
+        m.put(7, new Tuple<>(2, 3));
+        m.put(8, new Tuple<>(2, 4));
+        m.put(9, new Tuple<>(3, 1));
+        m.put(10, new Tuple<>(3, 2));
+        m.put(11, new Tuple<>(3, 3));
+        m.put(12, new Tuple<>(3, 4));
+        m.put(13, new Tuple<>(4, 1));
+        m.put(14, new Tuple<>(4, 2));
+        m.put(15, new Tuple<>(4, 3));
+        return m;
+
+    }
 
 }
---
+++ src/Tuple.java
@@ -0,0 +1,17 @@
+
+public class Tuple<S1, S2> {
+    private final S1 first;
+    private final S2 second;
+
+    public Tuple(S1 first, S2 second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public S1 getFirst() {
+        return first;
+    }
+
+    public S2 getSecond() {
+        return second;
+    }
